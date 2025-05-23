21c55a258cf9d4356c61b8d1da00d3ad992c317d
Joe Futrelle <jfutrelle@whoi.edu>
2025-05-23 08:58:50 -0400

    much cleanup

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ from prompts import DEFAULT_SYSTEM_PROMPT, DEFAULT_USER_PROMPT
 
 DATA_PREFIX = "./chromadb-data"
 
+DEFAULT_MODEL = 'qwen2.5vl'
+
 def get_client(persistent=False):
     if persistent:
         return chromadb.PersistentClient(path=DATA_PREFIX)
@@ -23,7 +25,7 @@ def get_client(persistent=False):
 
 class ImageIndex(object):
 
-    def __init__(self, collection_name, prompt=DEFAULT_USER_PROMPT, system=DEFAULT_SYSTEM_PROMPT, skip_existing=True, model='llava-phi3'):
+    def __init__(self, collection_name, prompt=DEFAULT_USER_PROMPT, system=DEFAULT_SYSTEM_PROMPT, skip_existing=True, model=DEFAULT_MODEL):
         self.client = None
         self.collection_name = collection_name
         self.prompt = prompt
@@ -51,25 +53,24 @@ class ImageIndex(object):
     def search_images(self, query):
         return self.collection.query(query_texts=[query])
     
-
-def main_index(directory, prompt=DEFAULT_USER_PROMPT, system=DEFAULT_SYSTEM_PROMPT, model='llava-phi3'):
-    with ImageIndex('images', prompt=prompt, system=system, model=model) as index:
+    def index_directory(self, directory):
         pngs = glob(os.path.join(directory, '*.png'))
         jpgs = glob(os.path.join(directory, '*.jpg'))
         image_paths = [*pngs, *jpgs]
         for image_path in tqdm(image_paths):
+            if self.skip_existing and self.exists(image_path):
+                continue
             with open(image_path, 'rb') as f:
                 image_data = f.read()
             try:
-                description = index.index_image(image_path, image_data)
-                # print(f"Indexed {image_path}: {description}")
+                description = self.index_image(image_path, image_data)
+                print(f"Indexed {image_path}: {description}")
             except:
-                # print(f"Failed to index {image_path}")
+                print(f"Failed to index {image_path}")
                 pass
-
-def main_query(query):
-    with ImageIndex('images') as index:
-        results = index.search_images(query)
+    
+    def search(self, query):
+        results = self.search_images(query)
         # json.dump(results, sys.stdout, indent=2)
         top_hit_id = results['ids'][0][0]
         top_hit_document = results['documents'][0][0]
@@ -82,18 +83,18 @@ def main_query(query):
                 image = image.convert('RGB')
             image.save('./top_hit.jpg')
 
-def main(directory, query='image', prompt=DEFAULT_USER_PROMPT, system=DEFAULT_SYSTEM_PROMPT, model='llava-phi3'):
-    if directory:
-        main_index(directory, prompt=prompt, system=system, model=model)
-    main_query(query)
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--directory', type=str, help='Directory containing images to index', default=None)
-    parser.add_argument('--query', type=str, help='Query to search for', default='image')
-    parser.add_argument('--model', type=str, help='Model to use for description', default='llava-phi3')
+    parser.add_argument('--query', type=str, help='Query to search for', default=None)
+    parser.add_argument('--model', type=str, help='Model to use for description', default=DEFAULT_MODEL)
     parser.add_argument('--prompt', type=str, help='Prompt to use for description', default=DEFAULT_SYSTEM_PROMPT)
     parser.add_argument('--system', type=str, help='System prompt to use for description', default=DEFAULT_SYSTEM_PROMPT)
     parser.add_argument('--skip-existing', action='store_true', help='Skip images that are already indexed')
     args = parser.parse_args()
-    main(args.directory, args.query)
+
+    with ImageIndex('images', prompt=args.prompt, system=args.system, model=args.model, skip_existing=args.skip_existing) as index:
+        if args.directory:
+            index.index_directory(args.directory)
+        elif args.query:
+            index.search(args.query)
